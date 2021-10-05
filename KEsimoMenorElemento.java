bd3b7a746ec6f573ebcce859f2bf8315df2bdace
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2021-10-05 13:57:40 -0400

    Add files via upload

--- KEsimoMenorElemento.java
+++ KEsimoMenorElemento.java
@@ -1,37 +1,78 @@
+import java.util.Random;
 public class KEsimoMenorElemento{
+ 
+	//Encontra o K-ésimo elemento ordenando o vetor e retornando k
+	public static int kEsimoOrdenando(int[] vetor, int k){
+		int[] vetorOrdenado = Ordenacao.quickSort(vetor);
+		return vetorOrdenado[k];
+	}
+
+	//Encontra o K-ésimo elemento do vetor gerando um heap min com seus valores
+	//Remove valores até chegar em K, então retorna K que deverá ser o K-ésimo elemento
+	public static int kEsimoUsandoHeapMin(int[] vetor, int k){
+		MinHeap minHeap = new MinHeap(vetor.length);
+
+		//insere valores no heap
+		for(int i=0; i<vetor.length;i++){
+			minHeap.insert(vetor[i]);
+		}
+
+		//remove até chegar no K
+		for(int i=0; i<k;i++){
+			minHeap.remove();
+		}
+
+		return minHeap.remove();
+	}
+
 	public static int randomizedSelect(int[] vetor, int p, int r, int k){
 		if(p == r){
 			return vetor[p];
 		}
 		int q = randomizedPartition(vetor, p, r);
 		int i = q - p + 1;
-		if(k == i){
+		if(i == k){
 			return vetor[q];
 		}
-		else if(k<i){
-			return randomizedSelect(vetor, p, q-1,k);
+		else if(i<k){
+			return randomizedSelect(vetor, p, q-1,i);
 		}
 		else{
-			return randomizedSelect(vetor,q+1,r, k-1);
+			return randomizedSelect(vetor,q+1,r, i-k);
 		}
 	}
 
-	public static int randomizedPartition(int[] vetor, p, r){
-		i = random(p,r);
+
+
+	private static int partition(int[] vetor, int p, int r){
+		int x = vetor[r];
+		int i = p - 1;
+		for(int j = p; j<r-1; j++){
+			if(vetor[j]<= x){
+				i = i + 1;
+				int aux = vetor[i];
+				vetor[i] = vetor [j];
+				vetor[j] = aux;
+			}
+		}
+		int aux = vetor[i+1];
+		vetor[i+1] = vetor[r];
+		vetor[r] = aux;
+		return i+1;
+	}
+
+	private static int randomizedPartition(int[] vetor, int p, int r){
+		Random random = new Random();
+		int i = random.nextInt(r) + p;  //seleciona um numero aleatório em p e r
 		int aux = vetor[r];
 		vetor[r] = vetor[i];
 		vetor[i] = aux;
-		return partiton(vetor, p, r);
+		return partition(vetor, p, r);
 	}
 
-	public static int partition(int[] vetor, p, r){
-		x = vetor[r];
-		i = p - 1;
-		//continua na pagina 192
-		//for(int j = p; j<r-1; j++)
-	}
-
+	/*
 	public static int select(vetor[], p, r, i){
 		//buscar em https://www.ime.usp.br/~pf/analise_de_algoritmos/aulas/median.html
 	}
+	*/
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,60 @@
+import java.util.Arrays;
+import java.util.Random;
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Funcionou");
+		int vetorInteiros[] = {10,20,30,40,50,100,75};
+
+		System.out.println(Arrays.toString(vetorInteiros));
+
+		//System.out.println(KEsimoMenorElemento.randomizedSelect(vetorInteiros, 0, 6, 2));
+
+		//System.out.println(KEsimoMenorElemento.kEsimoOrdenando(vetorInteiros, 2));
+
+		//System.out.println(KEsimoMenorElemento.kEsimoUsandoHeapMin(vetorInteiros,2));
+
+		int max = 50;
+		Random random = new Random();
+
+
+		System.out.println("      n     |      k     |    kth     |  1-quickSort  |   2-minHeap   |");
+
+		for(int i = 10; i<=max;i=i+10){
+
+			int k = random.nextInt(i-1);
+			int[] vetorTeste = new int[i];
+
+			int kEsimo;
+
+			//Preenche o vetor com números inteiros aleatórios
+            for(int j=0; j<vetorTeste.length; j++){
+                vetorTeste[j] = random.nextInt();
+            }
+
+            //Testa usando ordenação quicksort
+            long tempoInicial = System.nanoTime();
+            kEsimo = KEsimoMenorElemento.kEsimoOrdenando(vetorTeste, k);
+            long tempoFinal = System.nanoTime();
+
+            long tempoOrdenacao = tempoFinal - tempoInicial;
+            tempoInicial = 0;
+            tempoFinal = 0;
+
+            String tempoOrdenacaoFormatado = String.format("%.6f",(double)tempoOrdenacao/1000000000);
+
+
+            //Testa usando heapMin
+            tempoInicial = System.nanoTime();
+            kEsimo = KEsimoMenorElemento.kEsimoUsandoHeapMin(vetorTeste, k);
+            tempoFinal = System.nanoTime();
+
+            long tempoHeap = tempoFinal - tempoInicial;
+            tempoInicial = 0;
+            tempoFinal = 0;
+
+            String tempoHeapFormatado = String.format("%.6f",(double)tempoHeap/1000000000);
+
+            System.out.println("     "+i+"          "+k+"        "+kEsimo+"       "+tempoOrdenacaoFormatado+"         "+tempoHeapFormatado);
+		}
+
 	}
 }
